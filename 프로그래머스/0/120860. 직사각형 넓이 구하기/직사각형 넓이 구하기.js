19e3858626a6710302dd846c94243f257bc8ca9a
Lilly1757 <seungji34@gmail.com>
2024-10-03 20:28:12 +0900

    feat: 문제풀이

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,7 +1,8 @@
 function solution(dots) {
-    const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
-    const width = x1 === x2 ? Math.abs(y1 - y2) : x1 === x3 ? Math.abs(y1 - y3) : Math.abs(y1 - y4);  
-    const height = y1 === y2 ? Math.abs(x1 - x2) : y1 === y3 ? Math.abs(x1 - x3) : Math.abs(x1 -x4);
+  const [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots;
 
-    return width * height
+  const width = Math.max(x1, x2, x3, x4) - Math.min(x1, x2, x3, x4);
+  const height = Math.max(y1, y2, y3, y4) - Math.min(y1, y2, y3, y4);
+
+  return width * height;
 }

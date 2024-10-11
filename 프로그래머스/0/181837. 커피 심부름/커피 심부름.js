e4c486fab02f54e0857dbfce6bdf7b48f8bcc32c
Seungji Han <155867590+Tori57@users.noreply.github.com>
2024-10-11 23:18:01 +0900

    [level 0] Title: 커피 심부름, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,14 +1,1 @@
-function solution(order) {
-   const americano = ["americano", "iceamericano", "americanoice", "hotamericano", "americanohot", "anything"]
-   const cafelatte = ["cafelatte" ,"icecafelatte", "cafelatteice", "hotcafelatte", "cafelattehot"]
-   let sum = 0
-   for(let item of order) {
-     if (americano.includes(item)){
-          sum +=4500
-     } else {
-          sum +=5000
-     }
-   }
-    
-    return sum
-}
+const solution = (order) => order.reduce((acc, cur) => acc + (cur.includes("latte") ? 5000 : 4500), 0)
